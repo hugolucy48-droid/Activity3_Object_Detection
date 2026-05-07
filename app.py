25289cff1fe3f02a7ee2bf3ddcae7b67837f3675
hugolucy48-droid <hugolucy48@gmail.com>
2026-05-07 18:04:35 +0800

    Refactor video frame callback for object counting

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
+from streamlit_webrtc import webrtc_streamer
 from ultralytics import YOLO
 import av
 import cv2
